b298699c1690b313fb01ec97aa9bf04fed34c72f
SergioRodrii <serocastro@gmail.com>
2025-09-29 15:39:44 -0500

    Commit inicial - creacion de proyecto

---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Edad { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
